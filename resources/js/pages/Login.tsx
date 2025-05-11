14c0209c620ba03a74b15dae78a3d9b677c028fd
Kevin Adam <adamkevin520@gmail.com>
2025-05-11 10:35:35 +0700

    chore: upgrade tailwindcss to v4

--- resources/js/pages/Login.tsx
+++ resources/js/pages/Login.tsx
@@ -59,7 +59,7 @@ export default function LoginPage() {
           />
 
           <button
-            className="rounded-md w-full bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+            className="rounded-md w-full bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
             type="submit"
             disabled={form.processing}
           >
@@ -78,11 +78,11 @@ const Input = ({ error, className, ...props }: InputProps) => {
 
   return (
     <div>
-      <div className="relative rounded">
+      <div className="relative rounded-sm">
         <input
           {...props}
           className={cn(
-            "block w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6",
+            "block w-full rounded-md border-0 py-1.5  shadow-xs ring-1 ring-inset focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6",
             !isError
               ? "text-gray-900 ring-gray-300 placeholder:text-gray-400 focus:ring-indigo-600"
               : "text-red-900 ring-red-300 placeholder:text-red-300 focus:ring-red-500 pr-10",
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./resources/**/*.{tpl,tsx,ts}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
-};
